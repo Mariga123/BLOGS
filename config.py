b82a9786e7d8670d882941adee4d4c2cb7e98e13
Mariga123 <johnmariga8@gmail.com>
2020-12-14 12:09:13 +0300

    create app configurations

---
+++ config.py
@@ -0,0 +1,40 @@
+import os
+
+
+class Config:
+    """
+    Parent config
+    """
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SECRET_KEY='mariga'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = 'johnmariga8@gmail.com'
+    MAIL_PASSWORD = 'ze11y@jones'
+
+    QUOTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    
+
+
+class ProdConfig(Config):
+    """
+    Production config
+    """
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class DevConfig(Config):
+    """
+    Development config
+    """
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:zelly@localhost/johnte'
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
