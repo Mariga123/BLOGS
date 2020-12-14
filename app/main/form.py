f1d52366ba46543d9d794fcb2c263b2b7e58f0cd
Mariga123 <johnmariga8@gmail.com>
2020-12-14 12:04:26 +0300

    crate a blueprint

---
+++ app/main/form.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField, SubmitField
+from wtforms.validators import Required
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Write a brief bio about you.', validators=[Required()])
+    submit = SubmitField('Save')
+
+
+class PitchForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    category = SelectField('Category',
+                           choices=[('Politics', 'politics'), ('Relationship', 'relationships'), ('Sports', 'sports')],
+                           validators=[Required()])
+    post = TextAreaField('Your Blog', validators=[Required()])
+    submit = SubmitField('POST')
+
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Leave a comment', validators=[Required()])
+    submit = SubmitField('Comment')
